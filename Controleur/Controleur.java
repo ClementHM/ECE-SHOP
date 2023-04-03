c34a652194e3cf68f7cce480c6fd70f7295d1958
Azaffran <achille.zaffran@edu.ece.fr>
2023-04-03 17:08:50 +0100

    jdbc

--- Controleur/Controleur.java
+++ Controleur/Controleur.java
@@ -1,4 +1,21 @@
 package Controleur;
+import java.sql.*;
 
 public class Controleur {
+    public static void main(String[] args) {
+        Controleur C1= new Controleur();
+        C1.lectureArticle();
+    }
+    void lectureArticle(){
+        String sql = "SELECT article FROM Shopping";
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Shopping", "root", "");
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            while (rs.next()) {
+                System.out.println(rs.getString("article"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
