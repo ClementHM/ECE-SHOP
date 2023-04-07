ea0374f2ec29589a07d761ef100e8cbd8e8c1b03
elgoatinho <118262905+elgoatinho@users.noreply.github.com>
2023-04-07 14:22:50 +0200

    base de données ok

--- Controleur/Jdbc.java
+++ Controleur/Jdbc.java
@@ -3,16 +3,32 @@ import java.sql.*;
 
 public class Jdbc {
 
-    void lectureEmployé(){
+    void lectureEmployé() {
         String sql = "SELECT* from employé";
-        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/eceshopping", "root", "abcdef");
-             Statement stmt  = conn.createStatement();
-             ResultSet rs    = stmt.executeQuery(sql)){
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/shopping", "root", "abcdef");
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
             while (rs.next()) {
                 System.out.println(rs.getString("email"));
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
+        }
+    }
+
+    void lectureArticle() {
+        String sql = "SELECT* from article";
+
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/shopping", "root", "abcdef");
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                System.out.println(rs.getString("nom"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+
+
         }
     }
 }
--- Controleur/Test.java
+++ Controleur/Test.java
@@ -4,5 +4,7 @@ public class Test {
     public static void main(String[] args) {
         Jdbc C1= new Jdbc();
         C1.lectureEmployé();
+        C1.lectureArticle();
+
     }
 }
