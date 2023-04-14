6140ddc91a5ac4019892d0c43d4a3990ec19cb6f
elgoatinho <118262905+elgoatinho@users.noreply.github.com>
2023-04-14 11:53:44 +0200

    hop

---
+++ Vue/Main.java
@@ -0,0 +1,23 @@
+package Vue;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        TabPane tabPane = new TabPane();
+        Tab tab = new Tab("Onglet");
+        tabPane.getTabs().add(tab);
+        Scene scene = new Scene(tabPane, 400, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
