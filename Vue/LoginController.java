5e4c6622a797ff7204af5c7677a1a851ce0d1a02
elgoatinho <118262905+elgoatinho@users.noreply.github.com>
2023-04-14 16:08:24 +0200

    onglet login et compte

---
+++ Vue/LoginController.java
@@ -0,0 +1,49 @@
+package Vue;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+
+    @FXML
+
+    private HBox page_login;
+
+    @FXML
+    private Button bt_connexion;
+    @FXML
+    private Label label_titre;
+
+    @FXML
+    private Label label_email;
+
+    @FXML
+    private Label label_mdp;
+
+    @FXML
+    private Label label_pasdecompte;
+
+    @FXML
+    private Label label_employé;
+
+    @FXML
+
+    private Button bt_logemploye;
+    @FXML
+    private ImageView img_ece;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        page_login.getBlendMode().getDeclaringClass();
+
+    }
+}
+
