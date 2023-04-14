5f10583932138b1ec04fc4b55e877fb174b59604
Azaffran <achille.zaffran@edu.ece.fr>
2023-04-14 22:31:55 +0200

    shop

--- Modele/Modele.java
+++ Modele/Modele.java
@@ -13,16 +13,16 @@ public class Modele {
         java.sql.Timestamp timestamp = new java.sql.Timestamp(date.getTime());
 
         ArticleDao article1 = new ArticleDao(daoFactory);
-        article1.getArticleById(12);
+        article1.getArticleById(2);
         article1.lister();
         //EmployéDao employé= new EmployéDao(daoFactory);
         //employé.getEmployeById(2);
         //employé.lister();
         CommandeDao commande = new CommandeDao(daoFactory);
-        //commande.CommandeClient(5);
+        commande.CommandeClient(5);
         DetailcoDao detailcoDao = new DetailcoDao(daoFactory);
         //detailcoDao.CommandeParArticle(5);
-        //commande.saveCommande(timestamp, 6, 23, 1);
+        //commande.saveCommande(timestamp, 5, 100, 1);
         ClientDao clientDao =new ClientDao(daoFactory);
         //clientDao.saveClient("nom", "prénom", "adresse", "ville", "pays","email");
 
