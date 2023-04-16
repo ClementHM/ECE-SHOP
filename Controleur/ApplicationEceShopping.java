86fdb917eadf17564c216b1f60fdbc552c2b29b6
elgoatinho <118262905+elgoatinho@users.noreply.github.com>
2023-04-16 18:20:09 +0200

    finito

--- Controleur/ApplicationEceShopping.java
+++ Controleur/ApplicationEceShopping.java
@@ -1,4 +1,5 @@
-package Vue;
+package Controleur;
+
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,15 +7,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class main extends Application {
+public class ApplicationEceShopping extends Application {
+
 
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Controleur/Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/Vue/Controleur/Login.fxml"));
         stage.setTitle("ECE Shopping");
         stage.setScene(new Scene(root,1200,700));
         stage.show();
+
+
 
 
     }
@@ -24,3 +28,4 @@ public class main extends Application {
 
     }
 }
+
---
+++ Vue/run.java
@@ -0,0 +1,26 @@
+package Vue;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class run extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Controleur/Login.fxml"));
+        stage.setTitle("ECE Shopping");
+        stage.setScene(new Scene(root,1200,700));
+        stage.show();
+
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+}
